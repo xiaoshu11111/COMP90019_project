a6e0f1857f10609883d59efbfc50a413a63c972b
Marshall <linkshady428@gmail.com>
2019-10-29 18:07:39 +1100

    IOS

---
+++ COMP90019_ios/Graffiti/PhotoViewController.swift
@@ -0,0 +1,296 @@
+//
+//  PhotoViewController.swift
+//  Graffiti
+//
+//  Created by Teng-Sheng Ho on 2019/9/9.
+//  Copyright © 2019 Mh. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import MapKit
+import CoreLocation
+import Firebase
+import Contacts
+
+
+class PhotoViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var locationText: UITextField!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var desView: UITextView!
+    @IBOutlet weak var tagView: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    
+    var image: UIImage?
+    var pickImage = UIImagePickerController()
+    var ref: DatabaseReference! = Database.database().reference()
+    let locationManager = CLLocationManager()
+    var imageID:String!
+    var uploadLabels:String!
+    var uploadCoordinates:String!
+    var isGraffiti = false
+    
+    override func viewDidLoad() {
+   
+        imageView.image = image
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationItem.hidesBackButton = false
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelAction(_ sender: Any) {
+        finishButton.isHidden = false
+        chooseButton.isHidden = false
+        uploadButton.isHidden = true
+        cancelButton.isHidden = true
+        desView.isEditable = true
+        tagView.isEnabled = true
+        desView.backgroundColor = UIColor.white
+        tagView.backgroundColor = UIColor.white
+        locationText.backgroundColor = UIColor.white
+
+        uploadLabels = ""
+    }
+    
+    @IBAction func finishAction(_ sender: Any) {
+        if(imageView.image == nil && !(desView.text.isEmpty) && !(tagView.text!.isEmpty) ){
+            let alertController = UIAlertController(title: "Error", message: "Select an image", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }else{
+            finishButton.isHidden = true
+            chooseButton.isHidden = true
+            uploadButton.isHidden = false
+            cancelButton.isHidden = false
+            desView.isEditable = false
+            tagView.isEnabled = false
+            desView.backgroundColor = UIColor.clear
+            tagView.backgroundColor = UIColor.clear
+            locationText.backgroundColor = UIColor.clear
+            labelImage()
+        }
+    }
+    
+    @IBAction func uploadAction(_ sender: Any) {
+        imageID = (self.ref?.child("Image").childByAutoId().key)
+        uploadMedia() { url in
+            guard url != nil else { return }
+            self.ref?.child("images").child(self.imageID).setValue([
+                "description"     : self.desView.text!,
+                "label"           : self.uploadLabels!,
+                "graffiti"        : self.isGraffiti.description,
+                "location"        : self.uploadCoordinates!,
+                "tag"             : self.tagView.text!,
+                "uuid"            : Auth.auth().currentUser?.uid as Any
+                ])
+        }
+        
+        
+    }
+    
+    
+    @IBAction func chooseAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { _ in
+            self.openGallary()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Image labelling
+    func labelImage(){
+        var wallL = false
+        var postL = false
+   
+        
+        self.uploadLabels = ""
+        let labeler = Vision.vision().onDeviceImageLabeler()
+        let visionImage = VisionImage(image: self.imageView.image!)
+        
+        labeler.process(visionImage) { (labels, error) in
+            guard error == nil, let labels = labels, !labels.isEmpty else {
+                
+                self.dismiss(animated: true, completion: nil)
+                return
+            }
+            for label in labels {
+                var confd:Double
+                self.uploadLabels += "\(label.text) - \(label.confidence!),"
+                confd = label.confidence as! Double
+                if label.text == "Wall" && confd > 0.8{
+                    wallL = true
+                }
+                if label.text == "Poster" && confd > 0.4 {
+                    postL = true
+                }
+            }
+            if self.uploadLabels != "" {
+                self.uploadLabels.removeLast()
+                self.uploadLabels += "."
+                if wallL && postL {
+                    self.isGraffiti = true
+                }
+                print(self.uploadLabels!)
+            }else{
+                
+            }
+            
+        }
+    }
+    
+    //MARK: - Location manager
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        self.uploadCoordinates = "\(locValue.latitude) \(locValue.longitude)"
+        // print(self.uploadCoordinates!)
+        
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
+        geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+        
+        // Place details
+        var placeMark: CLPlacemark!
+        placeMark = placemarks?[0]
+        
+        
+        self.locationText.text = placeMark.postalAddress!.street + ", " + placeMark.postalCode!
+        //print(placeMark.postalAddress?.subAdministrativeArea)
+                  // Complete address as PostalAddress
+        //self.locationText.text = placeMark.postalAddress as Any as? String//  Import Contacts
+        /*
+        
+        // Location name
+        placeMark.name
+
+        // Street address
+        self.locationText.text = placeMark.thoroughfare
+
+        // Country
+        placeMark.country
+        }*/
+            
+        })
+        
+    }
+    
+    
+    //MARK: - Open the camera
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
+            pickImage.sourceType = UIImagePickerController.SourceType.camera
+            //If you dont want to edit the photo then you can set allowsEditing to false
+            pickImage.allowsEditing = false
+            pickImage.delegate = self
+            self.present(pickImage, animated: true, completion: nil)
+        }
+        else{
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: - Choose image from gallery
+    func openGallary(){
+        pickImage.sourceType = UIImagePickerController.SourceType.photoLibrary
+        //If you dont want to edit the photo then you can set allowsEditing to false
+        pickImage.allowsEditing = false
+        pickImage.delegate = self
+        self.present(pickImage, animated: true, completion: nil)
+    }
+    
+    func uploadMedia(completion: @escaping (_ url: String?) -> Void) {
+        
+        let path = imageID + ".jpg"
+        let storageRef = Storage.storage().reference().child("images").child(path)
+        let newMetadata = StorageMetadata()
+        newMetadata.contentType = "image/jpeg";
+        
+        if let uploadData = imageView.image?.jpegData(compressionQuality: 0.4) {
+            storageRef.putData(uploadData,metadata: newMetadata) { (metadata, error) in
+                if error != nil {
+                    print("error")
+                    completion(nil)
+                } else {
+                    storageRef.downloadURL(completion: { (url, error) in
+                        print(url?.absoluteString as Any)
+                        completion(url?.absoluteString)
+                        
+                        let alert = UIAlertController(title: "Successful!", message: "Upload Succefully!", preferredStyle: UIAlertController.Style.alert )
+                    
+                       alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+                            self.navigationController?.popViewController(animated: true)
+                       }))
+                       self.present(alert, animated:true, completion: nil)
+                            
+                    })
+                    
+                    
+                }
+            }
+        }
+    }
+}
+
+//MARK: - UIImagePickerControllerDelegate
+
+extension PhotoViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            
+            self.imageView.image = UIImage.resizedaImage(image: editedImage, newHeight: 270)
+        }
+        
+        //Dismiss the UIImagePicker after selection
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.isNavigationBarHidden = false
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+//MARK: - Resize Image
+extension UIImage {
+    class func resizedaImage(image: UIImage, newHeight: CGFloat) -> UIImage {
+        let scale = newHeight / image.size.height
+        let newWidth = image.size.width * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
+
---
+++ COMP90019_ios/Graffiti/SignUpViewController.swift
@@ -0,0 +1,88 @@
+//
+//  SignUpViewController.swift
+//  Graffiti
+//
+//  Created by Teng-Sheng Ho on 2019/9/11.
+//  Copyright © 2019 Mh. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var pwField: UITextField!
+    @IBOutlet weak var rePwField: UITextField!
+    override func viewDidLoad() {
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //where to upload Name!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        if (pwField.text != rePwField.text ){
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please comfirm your re-type password!", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }else if nameField.text!.isEmpty{
+            let alertController = UIAlertController(title: "Error", message: "Name must be not empty!", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }else{
+            
+            Auth.auth().createUser(withEmail: emailField.text!, password: pwField.text!){ (user, error) in
+                if error == nil {
+                        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                        changeRequest?.displayName = self.nameField.text!
+                        changeRequest?.commitChanges { (error) in
+                            if error != nil{
+                                print("Wrong!!!!!")
+                            }
+                            
+                    }
+                        let alert = UIAlertController(title: "Successful!", message: "You are a member now!\n Please login", preferredStyle: UIAlertController.Style.alert )
+                    
+                       alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+                        
+                        
+                       self.navigationController?.popToRootViewController(animated: true)
+                       // let loginView = self.storyboard!.instantiateViewController(withIdentifier: "loginView") as! LoginViewController
+                        
+                        //self.navigationController?.pushToViewController(loginView, animated: true)
+                        
+                        
+                       }))
+                       self.present(alert, animated:true, completion: nil)
+                }
+                else{
+                    
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
